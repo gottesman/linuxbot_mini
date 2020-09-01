b2867451648d00313ee557b4ce08bacb3aa1c622
Gabriel Gonzalez <gottesman@users.noreply.github.com>
2020-09-01 16:55:39 -0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+define('certificate', '/var/www/YOURPUBLIC.pem' );
+define('ip', 'xxx.xxx.xxx.xxx');
+define('botname', 'username');
+define('botid', '123456789');
+define('token', botid . ':XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
+define('botadmin', '987654321');
+define('vip', ['123456789', '123456789']);
