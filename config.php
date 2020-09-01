72a13549263e7d9d1ec2d556b8014cd9600d8760
Gabriel Gonzalez <gottesman@users.noreply.github.com>
2020-09-01 17:00:22 -0600

    Update config.php

--- config.php
+++ config.php
@@ -6,3 +6,4 @@ define('botid', '123456789');
 define('token', botid . ':XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
 define('botadmin', '987654321');
 define('vip', ['123456789', '123456789']);
+define('weatherapi','XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'); // https://openweathermap.org/price
